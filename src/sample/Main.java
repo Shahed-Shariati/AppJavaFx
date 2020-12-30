7a41167a001f023bd950053b11469e34efe4f6f3
Shariati <sha.shariat87@gmail.com>
2020-12-30 23:18:36 +0330

    Login Page

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,14 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(@NotNull Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View/LoginPage.fxml"));
+        primaryStage.setTitle("ورود به برنامه");
+        primaryStage.setScene(new Scene(root, 430, 240));
         primaryStage.show();
     }
 
--- src/sample/View/LoginPage.java
+++ src/sample/View/LoginPage.java
@@ -1,4 +1,26 @@
 package sample.View;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
 public class LoginPage {
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    void pressBtnExit(ActionEvent event) {
+
+    }
+
+    public void pressBtnExit(javafx.event.ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
